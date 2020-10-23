0d6063bb59f9231a58878929d10f513b6e8c1875
wangxinleo <wangxin.leo@outlook.com>
2020-10-23 15:39:08 +0800

    - nodeServer - protobuf

---
+++ protobuf/protobuf.js
@@ -0,0 +1,15 @@
+const protobuf = require("protocol-buffers");
+const fs = require("fs");
+
+const message = protobuf(fs.readFileSync(__dirname + "/test.proto"));
+
+var buf = message.Test.encode({
+  num: 42,
+  payload: "hello word",
+});
+
+console.log(buf);
+
+const obj = message.Test.decode(buf);
+
+console.log(obj);
